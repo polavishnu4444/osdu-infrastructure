3e6366b6ade292195676d7fb0d79c8e3a4d8b828
Daniel Scholl <dascholl@microsoft.com>
2020-07-07 19:52:56 -0500

    Added modules required for AKS

---
+++ infra/modules/providers/azure/aks/main.tf
@@ -0,0 +1,97 @@
+//  Copyright © Microsoft Corporation
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+data "azurerm_resource_group" "main" {
+  name = var.resource_group_name
+}
+
+resource "random_id" "main" {
+  keepers = {
+    group_name = data.azurerm_resource_group.main.name
+  }
+
+  byte_length = 8
+}
+
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = lower("${var.name}")
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_log_analytics_solution" "main" {
+  solution_name       = "ContainerInsights"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+
+  workspace_resource_id = azurerm_log_analytics_workspace.main.id
+  workspace_name        = azurerm_log_analytics_workspace.main.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = var.name
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+
+  tags = var.resource_tags
+
+  dns_prefix         = var.dns_prefix
+  kubernetes_version = var.kubernetes_version
+
+  linux_profile {
+    admin_username = var.admin_user
+
+    ssh_key {
+      key_data = var.ssh_public_key
+    }
+  }
+
+  default_node_pool {
+    name            = "default"
+    node_count      = var.agent_vm_count
+    vm_size         = var.agent_vm_size
+    os_disk_size_gb = 30
+    vnet_subnet_id  = var.vnet_subnet_id
+  }
+
+  network_profile {
+    network_plugin     = var.network_plugin
+    network_policy     = var.network_policy
+    service_cidr       = var.service_cidr
+    dns_service_ip     = var.dns_ip
+    docker_bridge_cidr = var.docker_cidr
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  service_principal {
+    client_id     = var.service_principal_id
+    client_secret = var.service_principal_secret
+  }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = var.oms_agent_enabled
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
+    }
+  }
+}
